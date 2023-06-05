38744da7b67a099f9bf1b4814e6850e9507e4c15
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-06-05 16:53:55 +0300

    refactor: moved logic to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import FormAddContact from './FormAddContact';
-import { Container } from 'react-bootstrap';
+import { Alert, Container } from 'react-bootstrap';
 import Section from './Section';
 import Contacts from './Contacts';
 import { nanoid } from 'nanoid';
@@ -50,18 +50,24 @@ export default class App extends Component {
   };
 
   render() {
-    const { filter } = this.state;
+    const { filter, contacts } = this.state;
     return (
       <Container className="w-50 p-3">
         <Section title="Phonebook">
           <FormAddContact onSubmit={this.addContact} />
         </Section>
         <Section title="Contacts">
-          <Filter value={filter} onChange={this.onFilterChange} />
-          <Contacts
-            contacts={this.getFilteredContacts()}
-            onDelete={this.onDelete}
-          />
+          {contacts.length !== 0 ? (
+            <>
+              <Filter value={filter} onChange={this.onFilterChange} />
+              <Contacts
+                contacts={this.getFilteredContacts()}
+                onDelete={this.onDelete}
+              />
+            </>
+          ) : (
+            <Alert variant="info">Contact list is empty</Alert>
+          )}
         </Section>
       </Container>
     );
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { Alert, ListGroup } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
 import Contact from './Contact';
 
 const Contacts = ({ contacts, onDelete }) => {
   return (
-    <>
-      {contacts.length !== 0 ? (
         <ListGroup variant="flush">
           {contacts.map(contact => (
             <Contact key={contact.id} contact={contact} onDelete={onDelete} />
           ))}
         </ListGroup>
-      ) : (
-        <Alert variant="info">Contact list is empty</Alert>
-      )}
-    </>
   );
 };
 
@@ -25,6 +19,7 @@ Contacts.propTypes = {
       id: PropTypes.string.isRequired,
     })
   ),
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Contacts
